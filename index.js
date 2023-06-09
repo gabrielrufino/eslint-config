2724d5393b8e935f45678a3843a54e1b051c4819
Gabriel Rufino <contato@gabrielrufino.com>
2023-06-09 19:13:26 -0300

    New rule: 'import/order'

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ module.exports = {
   rules: {
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
+    'import/order': ['error'],
     'import/prefer-default-export': 'off',
     'no-use-before-define': 'warn',
     'sort-imports': ['error']
   },
+  plugins: [
+    'eslint-plugin-import'
+  ],
 }
