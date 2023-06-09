876fba28a385507fb64d5a98c019fa747957053a
Gabriel Rufino <contato@gabrielrufino.com>
2023-06-09 18:08:10 -0300

    Disable rule 'import/no-unresolved'

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: 'airbnb-base',
   rules: {
     'import/extensions': 'off',
+    'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
     'no-use-before-define': 'warn',
   },
