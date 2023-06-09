028adc97343f16ec9087250d396f9076057cd239
Gabriel Rufino <contato@gabrielrufino.com>
2023-06-09 17:56:48 -0300

    Warning rule 'no-use-before-define'

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: 'airbnb-base',
   rules: {
     'import/extensions': 'off',
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'warn',
   },
 }
