662a4d79d69ff9b253d17d96e6547a922912f259
Gabriel Rufino <contato@gabrielrufino.com>
2023-06-14 10:54:40 -0300

    Disable rule 'no-underscore-dangle'

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'global-require': 'off',
     'no-await-in-loop': 'off',
+    'no-underscore-dangle': 'off',
     'no-use-before-define': 'warn',
   },
   plugins: [
